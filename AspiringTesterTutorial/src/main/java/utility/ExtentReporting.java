2e827383003fcc239871ecb9c25500c168f266c6
paramita-Bhattacharya <paramitabhattacharya09@gmail.com>
2022-01-09 10:43:34 +1100

    committed

---
+++ AspiringTesterTutorial/src/main/java/utility/ExtentReporting.java
@@ -0,0 +1,27 @@
+package utility;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReporting {
+	
+	
+	public static ExtentReports  ext;
+	
+	public static ExtentReports getReportObject() {
+	
+	String path = System.getProperty("user.dir")+"\\reports\\index.html";
+	ExtentSparkReporter reporter = new 	ExtentSparkReporter(path);
+	reporter.config().setReportName("Web Testing Result");
+	reporter.config().setDocumentTitle("Automated Test Result Generated");
+	
+	
+	ext = new ExtentReports();
+	ext.attachReporter(reporter);
+	ext.setSystemInfo("Tester", "Paramita Bhattacharya");
+	
+	return ext;
+	}
+
+}
+
